ccaaec5d2672f266eb63298217236bdb40af0620
David Sánchez <davidesj97@gmail.com>
2022-05-12 23:39:03 -0500

    Conectando el servidor con mongoDB

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,31 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+const uri = 'mongodb+srv://db_user:alu1yLoSVgAqh9Qv@cluster0.o0gwi.mongodb.net/db_chat?retryWrites=true&w=majority'
+db.Promise = global.Promise;
+
+db.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log("[db] conexión correcta");
+  })
+  .catch(() => {
+    console.error("[db] conexión fallida");
+  })
 
 function addMessage(message){
-  list.push(message);
+  // list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessage() {
-  return list;
+async function getMessage() {
+  // return list;
+  const messages = await Model.find();
+  return messages;
+
 }
 
 module.exports = {
